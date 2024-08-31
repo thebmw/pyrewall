6d5b84f2b203a7d98a72a3c914e64edfcce32d90
Brynn Warner <brynn@brynnsoft.com>
2024-08-31 12:22:14 -0400

    Add Scratch Project

---
+++ scratch-test/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using libnftables;
+
+Console.WriteLine("Hello, World!");
+var nftables = new Nftables();
+Console.WriteLine("Created nftables instance");
+var result = nftables.RunCommand("list tables");
+Console.WriteLine("Ran command");
+Console.WriteLine(result.Output);
